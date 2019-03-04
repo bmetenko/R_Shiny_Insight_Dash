dbdab7296280b3fca8c0174c886b49db55bb8186
bmetenko <bohdanmetenko@outlook.com>
2019-03-03 23:46:27 -0500

    API and Plot Data ETL and Graphing Conditionals

--- Nasa_Insight_API.R
+++ Nasa_Insight_API.R
@@ -10,6 +10,7 @@ minDay <- min(as.numeric(insight$sol_keys))
 
 maxDay <- max(as.numeric(insight$sol_keys))
 
+insight$sol_keys <- NULL
 
 tempToF <- function(degreesC) {
   degreesF <- ((9/5*degreesC)+32)
@@ -23,9 +24,55 @@ tempToC <- function(degreesF) {
   return(degreesC)
 }
 
-`$`(`$`(insight, `87`), `AT`)$`av`
+# `$`(`$`(insight, `87`), `AT`)$`av`
 
 # tempToF(degreesC =insight$`82`$AT$av[1])
+
+countDays <- length(names(insight))
+
+insightdata <- NULL
+
+insightdata$x <- as.numeric(names(insight))
+
+
+for (j in 1:4) {
+  at <- NULL
+  for (i in 1:countDays) {
+    message(paste0(j, ":", i))
+    at2 <- insight[[i]][1]["AT"][["AT"]][[j]]
+    at <- c(at, at2)}
+  
+  varNames <- names(insight[[1]][1]["AT"][["AT"]])
+  eval(parse(text = paste0("insightdata$y",varNames[j],"<- at")))
+  # insightdata$y <- at
+}
+
+avShow = TRUE
+mnShow = TRUE
+mxShow = TRUE
+
+# myTemp = 25
+
+p <- ggplot() + 
+{if(avShow) geom_line(mapping = aes(x = insightdata$x, y = insightdata$yav), 
+                      size = 2, color = "yellow", alpha = 0.75)} + 
+{if(mnShow) geom_line(mapping = aes(x = insightdata$x, y = insightdata$ymn),
+                      size = 2, color = "red", alpha = 0.75)} + 
+{if(mxShow) geom_line(mapping = aes(x = insightdata$x, y = insightdata$ymx),
+                      size = 2, color = "blue", alpha =0.75)} + 
+  theme_minimal() + 
+  xlab("Earth Day") +
+  ylab("Temperature in degrees C") +
+  ggtitle("Air Temperature") + 
+  # scale_y_continuous(minor_breaks = seq(-20, 0, 1)) +
+  # scale_x_continuous(minor_breaks = seq(80, 100, 1)) +
+  theme(title =  element_text(hjust = 0),
+        plot.title = element_text(hjust = 0.5),
+        axis.title = element_text(face = "bold", size = 12),
+        axis.text = element_text(size = 15, angle = 45, face = "bold" ),
+        panel.background = element_rect(fill = "maroon"))
+
+print(p)
 
 
 write_json(insight,path = paste0("InsightOutput.", Sys.Date(),"-min,", minDay,"-max,",maxDay, ".json"))
